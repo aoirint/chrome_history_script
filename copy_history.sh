1cbe4906fdf0e90290a42e1bc937718f5ecd38fb
aoirint <aoirint@gmail.com>
2024-12-04 21:58:00 +0900

    quote

--- copy_history.sh
+++ copy_history.sh
@@ -6,4 +6,4 @@ DATE_STRING=$(date '+%Y-%m-%d_%H-%M-%S')
 
 mkdir -p work
 
-cp $HOME/.config/google-chrome/Default/History "work/Chrome_History_${DATE_STRING}.sqlite3"
+cp "$HOME/.config/google-chrome/Default/History" "work/Chrome_History_${DATE_STRING}.sqlite3"
